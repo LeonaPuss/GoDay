5062578257a0e9072f9c5d87fd2ad9c3f1ee4e2b
LeonaPuss <leaderyu22333@gmail.com>
2021-08-07 08:33:07 +0800

    Front

---
+++ LearnC/CPrimerPlus/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int dogs;
+	printf("How many dogs do you have?\n");
+	scanf("%d",&dogs);
+	printf("So you have %d dog(s)!\n",dogs);
+	return 0;
+}
